96e2af24662204b548a8972fe0c36e1ec9219164
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-06 12:59:47 -0500

    Add Compose Add view

--- openfactory/datafabric/app/infra/compose/compose_add.py
+++ openfactory/datafabric/app/infra/compose/compose_add.py
@@ -1,7 +1,10 @@
 """
-DataFabric Compose project add view
+DataFabric Docker Compose project add view
 """
 import os
+import yaml
+from python_on_whales import DockerClient
+from python_on_whales.exceptions import DockerException
 from sqlalchemy import select
 from flask import current_app
 from flask import render_template
@@ -13,10 +16,11 @@ from flask_login import login_required
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Regexp
+from wtforms.validators import DataRequired, Regexp, ValidationError
 from wtforms_sqlalchemy.fields import QuerySelectField
 
 from openfactory.datafabric.app import db
+from openfactory.utils import get_configuration
 from openfactory.models.compose import ComposeProject
 from openfactory.models.nodes import Node
 
@@ -37,8 +41,30 @@ class ComposeAddForm(FlaskForm):
                                           message='Please use single word with only alphanumeric characters')])
     description = StringField('Description (optional)')
     yml_file = FileField('Docker Compose YAML configuration file', validators=[FileRequired()])
-
     submit = SubmitField('Add Docker Compose Project')
+
+    def validate_name(form, field):
+        """ Validate that compose project name is unique """
+        if db.session.query(ComposeProject.id).filter_by(name=field.data).first() is not None:
+            raise ValidationError("This name is already in use")
+
+    def validate_yml_file(form, field):
+        """ Validate that uploaded file is a Docker Compose YAML file """
+        datastore_system = get_configuration('datastore_system')
+        if datastore_system is None:
+            raise ValidationError("Administrator needs to configure the 'datastore_system' variable")
+        try:
+            yaml.safe_load(field.data)
+        except (yaml.YAMLError, UnicodeDecodeError):
+            raise ValidationError("The file does not seem to be a YAML file")
+        field.data.seek(0)
+        compose_file = os.path.join(datastore_system, form.name.data + '.yml')
+        field.data.save(compose_file)
+        docker = DockerClient(compose_files=[compose_file])
+        try:
+            docker.compose.config()
+        except DockerException:
+            raise ValidationError("The file is not a valid Docker Compose config file")
 
 
 class ComposeAdd(MethodView):
@@ -50,6 +76,10 @@ class ComposeAdd(MethodView):
     methods = ["GET", "POST"]
 
     def get(self):
+        # checks if required configuration is set by administrator
+        if get_configuration('datastore_system') is None:
+            flash("Cannot create Docker Compose projects. Administrator needs first to configure the 'datastore_system' variable", "danger")
+            return redirect(url_for('infra.compose_list'))
         form = ComposeAddForm()
         return render_template("infra/compose/compose_add.html",
                                form=form,
@@ -59,8 +89,7 @@ class ComposeAdd(MethodView):
         form = ComposeAddForm()
         if form.validate_on_submit():
             f = form.yml_file.data
-            f.save(os.path.join(current_app.instance_path, 'docker_compose', form.name.data + '.yml'))
-            f.seek(0)   # required because of the previous save
+            f.seek(0)   # required because of the previous save in validation
             compose = ComposeProject(
                 node=form.node.data,
                 node_id=form.node.data.id,
@@ -70,9 +99,8 @@ class ComposeAdd(MethodView):
             )
             db.session.add_all([compose])
             db.session.commit()
-
             flash(f'Added new Docker Compose project {form.name.data}', "success")
-            return redirect(url_for('infra.nodes'))
+            return redirect(url_for('infra.compose_list'))
         else:
             flash('Cannot create the Docker Compose project. Some entries are not valid', "danger")
             return render_template("infra/compose/compose_add.html",
