5498d37f66f102868fbdf5d262f99f5a4176d186
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-09 12:15:23 -0500

    Add type field

--- openfactory/datafabric/app/main/models/notifications.py
+++ openfactory/datafabric/app/main/models/notifications.py
@@ -19,6 +19,7 @@ class Notification(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     message: Mapped[str] = mapped_column(String(80))
+    type: Mapped[str] = mapped_column(String(80))
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     user: Mapped['User'] = relationship(back_populates='notifications')
 
--- openfactory/datafabric/app/main/views/notificationview.py
+++ openfactory/datafabric/app/main/views/notificationview.py
@@ -5,5 +5,5 @@ class NotificationView(ModelView):
     """
     Admin View for User Notification Model
     """
-    form_columns = ['user', 'message']
-    column_list = ['id', 'user', 'message']
+    form_columns = ['user', 'message', 'type']
+    column_list = ['id', 'user', 'message', 'type']
