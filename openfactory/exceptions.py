0bd12bab2e6486d64e10e70aedf2ed956f8d793f
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-12 10:27:01 -0400

    Add OFAConfigurationException

--- openfactory/exceptions.py
+++ openfactory/exceptions.py
@@ -6,3 +6,8 @@ OpenFactory exceptions
 class DockerComposeException(Exception):
     """ Docker compose returned an error """
     pass
+
+
+class OFAConfigurationException(Exception):
+    """ Something is misconfigured """
+    pass
