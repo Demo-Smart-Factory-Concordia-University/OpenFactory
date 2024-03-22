a7b62fb1f47bb1601c5066a034a2e2ffc73d4abf
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-22 11:29:06 -0400

    Use ofa.agent.attach

--- openfactory/datafabric/app/main/rq_tasks/agent_up.py
+++ openfactory/datafabric/app/main/rq_tasks/agent_up.py
@@ -3,13 +3,12 @@ RQ Task to spin up a new MTConnect Agent
 """
 import docker
 from rq import get_current_job
-from pyksql.ksql import KSQL
-from httpx import HTTPError
 from paramiko.ssh_exception import SSHException
 from sqlalchemy.exc import PendingRollbackError
 from openfactory.exceptions import DockerComposeException
 import openfactory.config as config
-from openfactory.models.containers import DockerContainer, EnvVar
+import openfactory.ofa as ofa
+from openfactory.exceptions import OFAException
 from openfactory.datafabric.app import db
 from openfactory.datafabric.app.main.models.tasks import RQTask
 
@@ -18,6 +17,7 @@ def agent_up(agent, container, mtc_file, producer_cpus):
     """ Spins up an MTConnect Agent """
     job = get_current_job()
     rq_task = db.session.get(RQTask, job.get_id())
+    current_user = rq_task.user
 
     # Deploy Agent and container
     try:
@@ -34,7 +34,7 @@ def agent_up(agent, container, mtc_file, producer_cpus):
         db.session.commit()
         return False
 
-    rq_task.user.send_notification(f'MTConnect agent {agent.uuid} created successfully', "success")
+    rq_task.user.send_notification(f'MTConnect agent {agent.uuid} created successfully', "info")
 
     # Start agent
     try:
@@ -45,57 +45,19 @@ def agent_up(agent, container, mtc_file, producer_cpus):
         db.session.commit()
         return False
 
-    rq_task.user.send_notification(f'MTConnect agent {agent.uuid} started successfully', "success")
+    rq_task.user.send_notification(f'MTConnect agent {agent.uuid} started successfully', "info")
 
-    # Create ksqlDB table for device handeld by the agent
-    ksql = KSQL(config.KSQLDB)
+    # create and start producer
     try:
-        ksql._statement_query(f"""CREATE TABLE IF NOT EXISTS {agent.device_uuid.replace('-', '_')} AS
-                                      SELECT id,
-                                             LATEST_BY_OFFSET(value) AS value
-                                      FROM devices_stream
-                                      WHERE device_uuid = '{agent.device_uuid}'
-                                      GROUP BY id;""")
-    except HTTPError:
-        rq_task.user.send_notification(f"Could not connect to KSQLdb {config.KSQLDB}", "danger")
+        ofa.agent.attach(agent.uuid,
+                         producer_cpus,
+                         user_notification=lambda msg: current_user.send_notification(msg, 'info'))
+    except OFAException as err:
+        rq_task.user.send_notification(err, "danger")
         rq_task.complete = True
         db.session.commit()
         return False
 
-    # create producer
-    producer = DockerContainer(
-        node_id=agent.node.id,
-        node=agent.node,
-        image=config.MTCONNECT_PRODUCER_IMAGE,
-        name=agent.uuid.lower().replace("-agent", "-producer"),
-        environment=[
-            EnvVar(variable='KAFKA_BROKER', value=config.KAFKA_BROKER),
-            EnvVar(variable='KAFKA_PRODUCER_UUID', value=agent.uuid.upper().replace('-AGENT', '-PRODUCER')),
-            EnvVar(variable='MTC_AGENT', value=f"{agent.agent_url}:{agent.agent_port}"),
-            EnvVar(variable='MTC_AGENT_UUID', value='agent_uuid')
-        ],
-        cpus=float(producer_cpus)
-    )
-    agent.producer_container = producer
-    try:
-        client.images.pull(config.MTCONNECT_PRODUCER_IMAGE)
-        db.session.add_all([producer])
-        db.session.commit()
-    except (DockerComposeException, PendingRollbackError, SSHException) as err:
-        db.session.rollback()
-        rq_task.user.send_notification(f'Producer for agent {agent.uuid} could not be created. Error was:<br>"{err}"', "danger")
-        rq_task.complete = True
-        db.session.commit()
-        return False
-
-    rq_task.user.send_notification(f'Producer for agent {agent.uuid} created successfully', "success")
-
-    # Start prodcuer
-    producer.start()
-    rq_task.user.send_notification(f'Producer for agent {agent.uuid} started successfully', "success")
-
-    client.close()
     rq_task.complete = True
-    db.session.commit()
     rq_task.user.send_notification(f'MTConnect agent {agent.uuid} was deployed successfully on {agent.node}', "success")
     return True
