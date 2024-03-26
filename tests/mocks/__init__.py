a0a822180aa98835c7fcc86264da25540597bd47
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-26 16:26:31 -0400

    Mock docker containers

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -2,5 +2,7 @@ from .mock_docker import docker_swarm
 from .mock_docker import docker_node
 from .mock_docker import docker_nodes
 from .mock_docker import docker_networks
+from .mock_docker import docker_container
+from .mock_docker import docker_containers
 from .mock_docker import docker_client
 from .mock_docker import docker_apiclient
