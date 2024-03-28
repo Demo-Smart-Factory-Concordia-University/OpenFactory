cd35b149fc34e8b325fa55a4f62015b17df378ab
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-28 09:19:47 -0400

    Use DataAccessLayer

--- openfactory/ofa/agent/ls.py
+++ openfactory/ofa/agent/ls.py
@@ -1,18 +1,14 @@
 import click
-from sqlalchemy import create_engine
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-import openfactory.config as config
 from openfactory.models.agents import Agent
+from openfactory.ofa.db import db
 
 
 @click.command(name='ls')
 def ls():
     """ List MTConnect agents defined in OpenFactory """
-    db_engine = create_engine(config.SQL_ALCHEMY_CONN)
-    session = Session(db_engine)
     agents = select(Agent)
     print('UUID            URL             PORT     STATUS         ATTACHED')
-    for agent in session.scalars(agents):
+    for agent in db.session.scalars(agents):
         print(f'{agent.uuid:15} {agent.agent_url:15} {agent.agent_port:<8d} {agent.status}        {agent.attached}')
