54f1865f70913d801805c40a165cebd86da0c016
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-31 22:32:41 -0400

    Use agent object

--- openfactory/ofa/agent/stop.py
+++ openfactory/ofa/agent/stop.py
@@ -1,39 +1,35 @@
 import click
 import requests
-from sqlalchemy import create_engine
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-import openfactory.config as config
+from openfactory.ofa.db import db
 from openfactory.models.agents import Agent
-from openfactory.models.containers import DockerContainer
 
 
-def stop(agent_uuid):
+def stop(agent):
     """ Stop an MTConnect agent defined in OpenFactory """
-    db_engine = create_engine(config.SQL_ALCHEMY_CONN)
-    session = Session(db_engine)
-    agents = select(Agent).where(Agent.uuid == agent_uuid)
-    for agent in session.scalars(agents):
-        if agent.external:
-            print("This is an external agent. It cannot be stoped by OpenFactory")
-            return
-        if not agent.status == 'running':
-            return
+    if agent.external:
+        print("This is an external agent. It cannot be stoped by OpenFactory")
+        return
+    if not agent.status == 'running':
+        return
 
-        # send agent_avail=UNAVAILABLE via MTConnect agent
-        url = f"http://{agent.agent_url}:{agent.agent_port}/Agent"
-        requests.post(url, data={'agent_avail': 'UNAVAILABLE'})
+    # send agent_avail=UNAVAILABLE via MTConnect agent
+    url = f"http://{agent.agent_url}:{agent.agent_port}/Agent"
+    requests.post(url, data={'agent_avail': 'UNAVAILABLE'})
 
-        # stop agent
-        query = select(DockerContainer).where(DockerContainer.name == agent_uuid.lower())
-        agent_cont = session.execute(query).one()
-        agent_cont[0].stop()
-        print("Stopped", agent_uuid)
+    # stop agent
+    agent.agent_container.stop()
+    print("Stopped", agent.uuid)
 
 
 @click.command(name='stop')
 @click.argument('agent_uuid', nargs=1)
 def click_stop(agent_uuid):
     """ Stop an MTConnect agent defined in OpenFactory """
-    stop(agent_uuid)
+    query = select(Agent).where(Agent.uuid == agent_uuid)
+    agent = db.session.execute(query).one_or_none()
+    if agent is None:
+        print(f'No Agent {agent_uuid} defined in OpenFactory')
+    else:
+        stop(agent[0])
