ea94b8f1b29b6bbf3c6831b6562fd302a1cfce68
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-03-31 22:14:54 -0400

    Use openfactory.ofa.db

--- openfactory/ofa/agent/start.py
+++ openfactory/ofa/agent/start.py
@@ -5,26 +5,26 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 import openfactory.config as config
+from openfactory.ofa.db import db
 from openfactory.models.agents import Agent
 
 
-def start(agent_uuid):
+def start(db_engine, agent_uuid):
     """ Start an MTConnect agent defined in OpenFactory """
-    db_engine = create_engine(config.SQL_ALCHEMY_CONN)
-    session = Session(db_engine)
-    agents = select(Agent).where(Agent.uuid == agent_uuid)
-    for agent in session.scalars(agents):
-        if agent.external:
-            print("This is an external agent. It cannot be started by OpenFactory")
-            return
-        client = docker.DockerClient(base_url="ssh://" + config.OPENFACTORY_USER + "@" + agent.agent_url)
-        container = client.containers.get(agent.container)
-        container.start()
-        print("Started ", agent_uuid)
+    query = select(Agent).where(Agent.uuid == agent_uuid)
+    agent = session.execute(query).one_or_none()
+    agent = agent[0]
+    if agent.external:
+        print("This is an external agent. It cannot be started by OpenFactory")
+        return
+    client = docker.DockerClient(base_url="ssh://" + config.OPENFACTORY_USER + "@" + agent.agent_url)
+    container = client.containers.get(agent.container)
+    container.start()
+    print("Started ", agent_uuid)
 
 
 @click.command(name='start')
 @click.argument('agent_uuid', nargs=1)
 def click_start(agent_uuid):
     """ Start an MTConnect agent defined in OpenFactory """
-    start(agent_uuid)
+    start(db.session, agent_uuid)
