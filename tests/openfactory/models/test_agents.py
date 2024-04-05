6780b29f21125966a01fb6776d1590a9ac09af46
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-04-05 11:02:12 -0400

    Add test_attached

--- tests/openfactory/models/test_agents.py
+++ tests/openfactory/models/test_agents.py
@@ -223,6 +223,39 @@ class TestAgent(TestCase):
         self.session.delete(node)
         self.session.commit()
 
+    def test_attached(self, *args):
+        """
+        Test hybrid_property 'attached'
+        """
+        agent = Agent(uuid='test-agent',
+                      agent_port=6000)
+        node = Node(
+            node_name='manager',
+            node_ip='123.456.7.891',
+            network='test-net'
+        )
+        agent.node = node
+        self.session.add_all([agent])
+        self.session.commit()
+
+        device_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                   'mocks/mock_device.xml')
+        agent.create_container('123.456.7.500', 7878, device_file, 1)
+
+        # check attached property
+        self.assertEqual(agent.attached, 'no')
+
+        # attach producer
+        agent.create_producer()
+
+        # check attached property
+        self.assertEqual(agent.attached, 'yes')
+
+        # clean-up
+        self.session.delete(agent)
+        self.session.delete(node)
+        self.session.commit()
+
     def test_container_removed(self, *args):
         """
         Test if Docker container of agent is removed when agent removed
