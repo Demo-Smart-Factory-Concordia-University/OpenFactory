4d94e7af9326e6b84ace5c3104c814db453cea32
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-04-29 18:31:02 -0400

    Use create_infrastack factory

--- openfactory/datafabric/app/main/rq_tasks/add_stack.py
+++ openfactory/datafabric/app/main/rq_tasks/add_stack.py
@@ -5,7 +5,8 @@ from docker.errors import APIError
 from paramiko.ssh_exception import SSHException
 from sqlalchemy.exc import PendingRollbackError
 from rq import get_current_job
-import openfactory.ofa as ofa
+from openfactory.models.user_notifications import user_notify
+from openfactory.factories import create_infrastack
 from openfactory.exceptions import OFAConfigurationException
 from openfactory.datafabric.app import db
 from openfactory.datafabric.app.main.models.tasks import RQTask
@@ -17,12 +18,14 @@ def add_stack(stack_config_file):
     """
     job = get_current_job()
     rq_task = db.session.get(RQTask, job.get_id())
-    current_user = rq_task.user
+
+    # setup user notifications
+    user_notify.success = lambda msg: rq_task.user.send_notification(msg, "success")
+    user_notify.info = lambda msg: rq_task.user.send_notification(msg, "info")
+    user_notify.fail = lambda msg: rq_task.user.send_notification(msg, "danger")
+
     try:
-        ofa.stack.up(db.session,
-                     stack_config_file,
-                     user_notification=lambda msg: current_user.send_notification(msg, 'info'))
-        rq_task.user.send_notification('Infrastructure stack was added successfully', 'success')
+        create_infrastack(db.session, stack_config_file)
     except (OFAConfigurationException, APIError, SSHException, PendingRollbackError) as err:
         db.session.rollback()
         rq_task.user.send_notification(f'Infrastructure stack could not be setup. Error was:<br>"{err}"', 'danger')
