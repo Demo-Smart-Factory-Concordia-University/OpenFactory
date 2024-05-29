c1843db944cf384e9d29acc231d279d1a0e39fce
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-05-29 14:25:58 -0400

    Improve visual

--- openfactory/ofa/agent/ls.py
+++ openfactory/ofa/agent/ls.py
@@ -9,6 +9,6 @@ from openfactory.ofa.db import db
 def ls():
     """ List MTConnect agents defined in OpenFactory """
     agents = select(Agent)
-    print('UUID            URL             PORT     STATUS         ATTACHED')
+    print('UUID                      URL             PORT     STATUS         ATTACHED')
     for agent in db.session.scalars(agents):
-        print(f'{agent.uuid:15} {agent.agent_url:15} {agent.agent_port:<8d} {agent.status}        {agent.attached}')
+        print(f'{agent.uuid:25} {agent.agent_url:15} {agent.agent_port:<8d} {agent.status}        {agent.attached}')
