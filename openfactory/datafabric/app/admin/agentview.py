8091e62f6db85a6444ce8d97170678930ba0ff1d
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-08-15 23:18:29 -0400

    Add adapter IP and port

--- openfactory/datafabric/app/admin/agentview.py
+++ openfactory/datafabric/app/admin/agentview.py
@@ -6,9 +6,10 @@ class AgentView(ModelView):
     Admin View for Agent Model
     """
     form_columns = ['uuid', 'agent_port', 'node']
-    column_list = ['uuid', 'device_uuid', 'device_xml', 'agent_port', 'cpus_reservation', 'cpus_limit', 'container', 'node', 'status']
+    column_list = ['uuid', 'device_uuid', 'device_xml', 'agent_port', 'adapter_ip', 'adapter_port', 'cpus_reservation', 'cpus_limit', 'container', 'node', 'status']
     column_labels = {'uuid': 'Agent UUID',
                      'device_xml': 'Device xml model',
                      'cpus_reservation': 'CPUs reservation',
                      'cpus_limit': 'CPUs limit',
+                     'adapter_ip': 'Adapter IP',
                      'device_uuid': 'Device UUID'}
