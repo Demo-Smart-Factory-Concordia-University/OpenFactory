0b8a12c171bd83f801c445940e29887df10c8ac6
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-08-21 10:18:14 -0400

    Add label to node

--- openfactory/factories/create_infra.py
+++ openfactory/factories/create_infra.py
@@ -6,6 +6,22 @@ from openfactory.models.user_notifications import user_notify
 from openfactory.utils import load_yaml
 
 
+def add_label(node_ip, name):
+    """ Adds the lable `name` to the node with address `node_ip` """
+    node = None
+    for n in dal.docker_client.nodes.list():
+        if node_ip in n.attrs['Status']['Addr']:
+            node = n
+            break
+    if node is None:
+        return
+
+    # add label
+    node_spec = node.attrs['Spec']
+    node_spec['Labels'] = {'name': name}
+    node.update(node_spec)
+
+
 def create_managers(managers):
     """ Create manager nodes """
     for name, ip in managers.items():
@@ -16,6 +32,7 @@ def create_managers(managers):
             if 'Swarm' in info and info['Swarm']['NodeID']:
                 continue
             client.swarm.join([dal.ip], join_token=dal.manager_token)
+            add_label(ip, name)
             user_notify.success(f'Node "{name} ({ip})" setup')
         except (docker.errors.APIError) as err:
             user_notify.fail(f'Node "{name}" could not be setup. Error was:<br>"{err}"')
@@ -31,6 +48,7 @@ def create_workers(workers):
             if 'Swarm' in info and info['Swarm']['NodeID']:
                 continue
             client.swarm.join([dal.ip], join_token=dal.worker_token)
+            add_label(ip, name)
             user_notify.success(f'Node "{name} ({ip})" setup')
         except (docker.errors.APIError) as err:
             user_notify.fail(f'Node "{name}" could not be setup. Error was:<br>"{err}"')
