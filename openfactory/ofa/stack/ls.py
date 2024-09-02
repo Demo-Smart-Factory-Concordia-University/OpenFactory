5dd70cec043d989eff2c85f4141a762eb2d8bb65
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-09-02 17:14:11 -0400

    Update ls command

--- openfactory/ofa/stack/ls.py
+++ openfactory/ofa/stack/ls.py
@@ -1,8 +1,32 @@
 import click
-from sqlalchemy import select
+from datetime import datetime, timezone
+from openfactory.docker.docker_access_layer import dal
 
-from openfactory.models.infrastack import InfraStack
-from openfactory.ofa.db import db
+
+def print_services(node_id):
+    """ Print information on services running on a node """
+    services = dal.docker_client.services.list()
+    formatted_lines = []
+    for service in services:
+        tasks = dal.docker_client.api.tasks(filters={"service": service.name})
+        for task in tasks:
+            if task['NodeID'] == node_id and task['Status']['State'] == 'running':
+                ports = service.attrs.get('Endpoint', {}).get('Ports', [])
+                public_ports = ', '.join([str(port['PublishedPort']) for port in ports if 'PublishedPort' in port])
+                started_at_str = task['Status']['Timestamp']
+                trimmed_timestamp = started_at_str[:26] + 'Z'
+                started_at = datetime.strptime(trimmed_timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+                time_running = datetime.now(timezone.utc) - started_at
+                hours, remainder = divmod(time_running.total_seconds(), 3600)
+                minutes, _ = divmod(remainder, 60)
+                running_for = f"{int(hours)}h {int(minutes)}m ago" if hours else f"{int(minutes)}m ago"
+                formatted_lines.append(f"{service.name:25}  Running since {running_for:15} {public_ports}")
+
+    for i, line in enumerate(formatted_lines):
+        if i < len(formatted_lines) - 1:
+            print(f"  ├ {line}")
+        else:
+            print(f"  └ {line}")
 
 
 @click.command(name='ls')
@@ -10,12 +34,14 @@ from openfactory.ofa.db import db
               flag_value='verbose', default=False,
               help='Increase verbosity')
 def click_ls(verbose):
-    """ List infrastructure stacks defined in OpenFactory """
-    stacks = select(InfraStack)
-    for stack in db.session.scalars(stacks):
-        print(f'{stack.stack_name}:')
-        for node in stack.nodes:
-            print(f'  {node.node_name:15} {node.cpus:3} cpus    {node.status}')
-            if verbose:
-                for cont in node.containers:
-                    print(f'    • {cont.name:30} {cont.cpus:3} cpus   {cont.status}')
+    """ List OpenFactory nodes """
+    nodes = dal.docker_client.nodes.list()
+    for node in nodes:
+        cpus = node.attrs['Description']['Resources']['NanoCPUs'] / 1E9
+        ram = node.attrs['Description']['Resources']['MemoryBytes'] / (1024 ** 3)
+        node_type = 'Manager' if 'ManagerStatus' in node.attrs else 'Worker'
+        print(f"{node.attrs['Description']['Hostname']} {node.attrs['Status']['Addr']} ({cpus} CPUs {ram:.1f} GB) {node_type:8} {node.attrs['Spec']['Availability']:8} {node.attrs['Status']['State']}")
+        if verbose:
+            print("  SERVICE                      CURRENT STATE                 PUBLIC PORTS")
+            print_services(node.id)
+            print()
