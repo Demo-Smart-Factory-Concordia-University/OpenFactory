ba45799a62229a90a52d4ea347bb50399653f2cc
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-10-01 12:13:21 -0400

    Remove obsolte remove_stack rq_task

--- openfactory/datafabric/app/main/rq_tasks/__init__.py
+++ openfactory/datafabric/app/main/rq_tasks/__init__.py
@@ -7,7 +7,6 @@ from .node_up import node_up
 from .node_down import node_down
 from .compose_up import compose_up
 from .compose_down import compose_down
-from .remove_stack import remove_stack
 from .add_stack import add_stack
 from .agent_up import agent_up
 from .agent_down import agent_down
