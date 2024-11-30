b4d410029b0db37b36ff5f514a3e91b5b42ec3ca
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-11-29 19:17:13 -0500

    Remove obsolete node and compose models

--- openfactory/models/__init__.py
+++ openfactory/models/__init__.py
@@ -5,5 +5,3 @@ OpenFactory models module
 from .session_events import receive_persistent_to_deleted
 from .configurations import Configuration
 from .agents import Agent
-from .compose import ComposeProject
-from .nodes import Node
--- openfactory/models/session_events.py
+++ openfactory/models/session_events.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 
 from .user_notifications import user_notify
 from .agents import Agent
-from .nodes import Node
 
 
 @event.listens_for(Session, 'persistent_to_deleted')
@@ -13,5 +12,3 @@ def receive_persistent_to_deleted(session, instance):
     """
     if isinstance(instance, Agent):
         user_notify.success(f"Agent {instance.uuid} removed successfully")
-    if isinstance(instance, Node):
-        user_notify.success(f"Node '{instance.node_name}' removed successfully")
