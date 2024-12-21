a7aa176b9912a11144799b3965d9d82c327b6e8a
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2024-12-21 17:43:17 -0500

    Add ofacmds.sql

--- Kafka-Cluster/scripts/ofacmds.sql
+++ Kafka-Cluster/scripts/ofacmds.sql
@@ -1,11 +1,11 @@
--- OpenFactory cmd stream
+-- OpenFactory cmds stream
 CREATE STREAM cmds_stream (
-    device_uuid VARCHAR KEY,
-    cmd VARCHAR,
-    args VARCHAR
-) WITH (
-    KAFKA_TOPIC = 'ofa_cmds',
-    PARTITIONS = 1,
-    REPLICAS = 1,
-    VALUE_FORMAT = 'JSON'
-);
+        device_uuid VARCHAR KEY,
+        cmd VARCHAR,
+        args VARCHAR
+    ) WITH (
+        KAFKA_TOPIC = 'ofa_cmds',
+        PARTITIONS = 1,
+        REPLICAS = 1,
+        VALUE_FORMAT = 'JSON'
+    );
--- Kafka-Cluster/scripts/start_ksql.sh
+++ Kafka-Cluster/scripts/start_ksql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for script in ${KSQL_START_SCRIPTS//:/ }; do
+    echo "Running script "$script
     ksql http://ksqldb-server:8088 --file scripts/$script
 done
