5f8c633c348b1f3f26c248bf754df0a59d99de54
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-01-10 11:21:21 -0500

    General revision

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
-# install in your virtualenv with
-# pip install --editable .
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='ofa',
+    name='openfactory',
     version='0.1.0',
-    py_modules=['ofa'],
+    packages=find_packages(include=["openfactory", "openfactory.*"]),
+    include_package_data=True,
     install_requires=[
-        'Click',
+        "paramiko",
+        "docker",
+        "pyyaml",
+        "click",
+        "sqlalchemy",
+        "flake8",
+        "python-dotenv",
+        "PyGithub",
+        "fsspec",
+        "influxdb-client",
+        "asyncua",
+        "git+https://github.com/rwuthric/PyKSQL.git",
+        "git+https://github.com/rwuthric/python-mtc2kafka.git",
+        # DataFabric WebApp dependencies
+        "flask",
+        "flask-sqlalchemy",
+        "flask-admin",
+        "flask-login",
+        "flask-wtf",
+        "WTForms-SQLAlchemy",
+        "python-on-whales",
+        "rq"
     ],
-    entry_points={
-        'console_scripts': [
-            'ofa = ofa:cli',
-        ],
-    },
 )
