eb835897c3a9b3c691ff4028f81a06670d088850
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-01-10 11:31:43 -0500

    Use requirements.txt

--- setup.py
+++ setup.py
@@ -1,32 +1,13 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename) as file:
+        return file.read().splitlines()
+
 setup(
     name='openfactory',
     version='0.1.0',
     packages=find_packages(include=["openfactory", "openfactory.*"]),
     include_package_data=True,
-    install_requires=[
-        "paramiko",
-        "docker",
-        "pyyaml",
-        "click",
-        "sqlalchemy",
-        "flake8",
-        "python-dotenv",
-        "PyGithub",
-        "fsspec",
-        "influxdb-client",
-        "asyncua",
-        "git+https://github.com/rwuthric/PyKSQL.git",
-        "git+https://github.com/rwuthric/python-mtc2kafka.git",
-        # DataFabric WebApp dependencies
-        "flask",
-        "flask-sqlalchemy",
-        "flask-admin",
-        "flask-login",
-        "flask-wtf",
-        "WTForms-SQLAlchemy",
-        "python-on-whales",
-        "rq"
-    ],
+    install_requires=parse_requirements('requirements.txt'),
 )
