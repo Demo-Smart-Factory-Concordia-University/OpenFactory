4496bc548847505d0c878e06690c79e145792f41
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-02-04 10:39:41 -0500

    Handle producer shutdown

--- MTConnect/MTC2Kafka/kafka_producer.py
+++ MTConnect/MTC2Kafka/kafka_producer.py
@@ -1,4 +1,5 @@
 import os
+import signal
 from mtc2kafka.connectors import MTCSourceConnector
 
 
@@ -10,6 +11,18 @@ class MTC_Producer(MTCSourceConnector):
     kafka_producer_uuid = os.environ['KAFKA_PRODUCER_UUID']
 
 
+def signal_handler(sig, frame):
+    """ Handles shutdown of producer """
+    print(f"Received signal {sig}; cleaning-up ...")
+    con.send_producer_availability("UNAVAILABLE")
+    exit(0)
+
+
+# Register signal handlers
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+
+# Setup producer
 con = MTC_Producer()
 print("======================================================")
 print("Kafka producer for MTConnect data from", con.mtc_agent)
