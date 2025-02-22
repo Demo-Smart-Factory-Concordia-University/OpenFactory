d9add12041d59c7ed5db189e5c46cbae45029df8
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-02-22 18:49:40 -0500

    Add get_nested

--- openfactory/utils/__init__.py
+++ openfactory/utils/__init__.py
@@ -6,3 +6,12 @@ from ..config import load_yaml
 from .docker_compose_up import docker_compose_up
 from .open_uris import open_ofa
 from .assets import register_asset,deregister_asset
+
+def get_nested(data, keys, default=None):
+    """ Get safely a nested value from a dictionary """
+    for key in keys:
+        if isinstance(data, dict):
+            data = data.get(key, default)
+        else:
+            return default
+    return data
