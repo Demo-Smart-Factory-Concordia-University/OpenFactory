46d081cc0fc0b816b0e1d7ace68235b47bde0d05
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-03-21 19:46:29 -0400

    Fix import

--- openfactory/kafka/__init__.py
+++ openfactory/kafka/__init__.py
@@ -1,3 +1,3 @@
 from .asset_consumer import KafkaAssetConsumer, CaseInsensitiveDict
 from .delete_consumer_group import delete_consumer_group
-from .commands_consumer import KafkaCommandsConsumer
+#from .commands_consumer import KafkaCommandsConsumer
