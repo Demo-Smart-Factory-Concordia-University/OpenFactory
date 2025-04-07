2afec6107509d94d5d2b49843a0d342d61ee568e
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-04-07 16:52:42 -0400

    Rename

--- openfactory/factories/__init__.py
+++ openfactory/factories/__init__.py
@@ -1,5 +1,5 @@
 from .deploy_devices import deploy_devices_from_config_file
-from .shut_down_devices_from_config_file import shut_down_devices_from_config_file
+from .shut_down_devices import shut_down_devices_from_config_file
 from .deploy_apps import deploy_apps_from_config_file
 from .shut_down_apps import shut_down_apps_from_config_file
 from .create_infra import create_infrastack
