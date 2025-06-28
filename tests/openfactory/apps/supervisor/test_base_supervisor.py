9ce912b7b8ddb04ed6f261d6ffc032f660a74603
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-06-28 12:58:45 -0400

    Fix AssetProducer patch

--- tests/openfactory/apps/supervisor/test_base_supervisor.py
+++ tests/openfactory/apps/supervisor/test_base_supervisor.py
@@ -26,7 +26,7 @@ class BaseSupervisorTestCase(unittest.TestCase):
         self.ksql_mock = MagicMock()
 
         # Patch AssetProducer
-        self.asset_producer_patcher = patch("openfactory.assets.asset_class.AssetProducer")
+        self.asset_producer_patcher = patch("openfactory.assets.asset_base.AssetProducer")
         self.MockAssetProducer = self.asset_producer_patcher.start()
         self.addCleanup(self.asset_producer_patcher.stop)
 
