1ba02ff6b1124cc4b648c5ac49db26b461cb6ff7
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-06-29 00:57:12 -0400

    Fix patch

--- tests/openfactory/test_openfactory.py
+++ tests/openfactory/test_openfactory.py
@@ -13,7 +13,7 @@ class TestOpenFactory(TestCase):
         self.ksql_mock = MagicMock()
 
         # Patch AssetProducer
-        self.asset_producer_patcher = patch("openfactory.assets.asset_class.AssetProducer")
+        self.asset_producer_patcher = patch("openfactory.assets.asset_base.AssetProducer")
         self.MockAssetProducer = self.asset_producer_patcher.start()
         self.addCleanup(self.asset_producer_patcher.stop)
 
