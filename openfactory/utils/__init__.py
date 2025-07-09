63c2db6ed29cf0816f2eb9624898017881b915c3
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-07-09 18:15:37 -0400

    Fix return type hint

--- openfactory/utils/__init__.py
+++ openfactory/utils/__init__.py
@@ -2,14 +2,14 @@
 
 import glob
 import os
-from typing import Dict, List
+from typing import Dict, List, Any
 from openfactory.config import load_yaml
 from openfactory.utils.docker_compose_up import docker_compose_up
 from openfactory.utils.open_uris import open_ofa
 from openfactory.utils.assets import register_asset,deregister_asset
 
 
-def get_nested(data: Dict, keys, default=None) -> any:
+def get_nested(data: Dict, keys, default=None) -> Any:
     """
     Get safely a nested value from a dictionary.
 
